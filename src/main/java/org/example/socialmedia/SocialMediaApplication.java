b389c31b5573be59cbecad22a4d721106d6aad51
Xushnud <xushnudxurramov9@gmail.com>
2024-10-24 16:03:17 +0500

    a

--- src/main/java/org/example/socialmedia/SocialMediaApplication.java
+++ src/main/java/org/example/socialmedia/SocialMediaApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SocialMediaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SocialMediaApplication.class, args);
     }
+
 
 }
---
+++ src/main/java/org/example/socialmedia/entity/User.java
@@ -0,0 +1,28 @@
+package org.example.socialmedia.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String name;
+    private String surname;
+    private String password;
+    private String phoneNumber;
+    private String email;
+
+
+}
