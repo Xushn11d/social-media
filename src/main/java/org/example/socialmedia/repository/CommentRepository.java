b11846c7ab925d1972c472d6c54bdfa0265225d8
webuza <webuza101@gmail.com>
2024-10-24 16:14:19 +0500

    creat

---
+++ src/main/java/org/example/socialmedia/repository/CommentRepository.java
@@ -0,0 +1,8 @@
+package org.example.socialmedia.repository;
+
+import org.example.socialmedia.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+}
